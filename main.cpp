d3430efd90ecfe4b290eb1bef725de3e1cd2e519
phoebe <phoebe@PHOEBE-PC>
2023-08-13 11:37:24 -0700

    First PPM image with progress bar

---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+int main() {
+
+    // Image
+    int image_width = 256;
+    int image_height = 256;
+
+    // Render
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    // Write pixels left to right, top to bottom
+    for (int j = 0; j < image_height; ++j) {
+        
+        // Progress indicator
+        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
+
+        for (int i = 0; i < image_width; ++i) {
+            // RGB in range [0, 1]
+            auto r = double(i) / (image_width-1);
+            auto g = double(j) / (image_height-1);
+            auto b = 0;
+
+            // Scale to integers [0, 255]
+            int ir = static_cast<int>(255.999 * r);
+            int ig = static_cast<int>(255.999 * g);
+            int ib = static_cast<int>(255.999 * b);
+
+            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+        }
+
+        std::clog << "\rDone.                 \n";
+    }
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
