3e1aa3288df429d6869968454fd75f91012f3a0a
phoebe <phoebe@PHOEBE-PC>
2023-08-14 21:13:17 -0700

    Surface normal shading

--- main.cpp
+++ main.cpp
@@ -4,18 +4,25 @@
 
 #include <iostream>
 
-bool hit_sphere(const point3 &center, double radius, const ray &r) {
+double hit_sphere(const point3 &center, double radius, const ray &r) {
     vec3 oc = r.origin() - center;
     double a = dot(r.direction(), r.direction());
     double b = 2.0 * dot(oc, r.direction());
     double c = dot(oc, oc) - radius * radius;
     auto discriminant = b*b - 4*a*c; // If negative, there are no real solutions
-    return (discriminant >= 0);
+    
+    if (discriminant < 0) {
+        return -1.0;
+    } else {
+        return (-b - sqrt(discriminant)) / (2.0 * a); // Quadratic formula
+    }
 }
 
 color ray_color(const ray &r) {
-    if (hit_sphere(point3(0, 0, -1), 0.5, r)) {
-        return color(1, 0, 0);
+    double t = hit_sphere(point3(0, 0, -1), 0.5, r);
+    if (t > 0.0) {
+        vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1)); // Vector from sphere center to hit point
+        return 0.5 * color(N.x()+1, N.y()+1, N.z()+1); // Scale unit vector components from [-1, 1] to [0, 1]
     }
 
     vec3 unit_direction = unit_vector(r.direction()); // y ranges [-1, 1]
