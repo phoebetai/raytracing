241b73da47b8d5ad623d0419042e74f74b90a96b
phoebe <phoebe@PHOEBE-PC>
2023-09-04 16:12:38 -0700

    Simplify ray-sphere intersection code

--- main.cpp
+++ main.cpp
@@ -6,15 +6,15 @@
 
 double hit_sphere(const point3 &center, double radius, const ray &r) {
     vec3 oc = r.origin() - center;
-    double a = dot(r.direction(), r.direction());
-    double b = 2.0 * dot(oc, r.direction());
-    double c = dot(oc, oc) - radius * radius;
-    auto discriminant = b*b - 4*a*c; // If negative, there are no real solutions
+    double a = r.direction().length_squared();
+    double half_b = dot(oc, r.direction());
+    double c = oc.length_squared() - radius * radius;
+    double discriminant = half_b * half_b - a * c; // If negative, there are no real solutions
     
     if (discriminant < 0) {
         return -1.0;
     } else {
-        return (-b - sqrt(discriminant)) / (2.0 * a); // Quadratic formula
+        return (-half_b - sqrt(discriminant)) / (2.0 * a); // Quadratic formula
     }
 }
 
