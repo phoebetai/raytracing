bcd6e3c0dcef69965bb59a958313d04e94643ab1
phoebe <phoebe@PHOEBE-PC>
2023-09-04 18:14:47 -0700

    Add surface side determination

--- hittable.h
+++ hittable.h
@@ -8,6 +8,14 @@ class hit_record {
 		point3 p;
 		vec3 normal;
 		double t;
+		bool front_face;
+
+		void set_face_normal(const ray &r, const vec3 &outward_normal) {
+			// Sets the hit record normal vector
+			// The parameter outward_normal is assumed to have unit length
+			front_face = dot(r.direction(), outward_normal) < 0;
+			normal = front_face ? outward_normal : -outward_normal;
+	}
 };
 
 class hittable {
@@ -15,7 +23,7 @@ class hittable {
 		virtual ~hittable() = default;
 
 		// Hit is only valid if tmin < t < tmax
-		virtual bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const = 0;
+		virtual bool hit(const ray &r, double ray_tmin, double ray_tmax, hit_record &rec) const = 0;
 };
 
 #endif
